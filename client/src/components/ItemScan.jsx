18f2a00a3adffd80379b44eb290d0522ba7dea91
Arad Ben Menashe <aradbm@gmail.com>
2023-05-11 15:46:01 +0300

    Update ItemScan.jsx

---
+++ client/src/components/ItemScan.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import Axios from "axios";
+import { useState } from "react";
+
+export const ItemScan = () => {
+  const [data, setData] = useState("");
+  const [input, setInput] = useState("");
+  const fetchData = (props) => {
+    Axios.get(`http://localhost:5001/api/${props}`).then((response) => {
+      setData(response.data);
+      console.log(response.data);
+    });
+  };
+  return (
+    <Box sx={{ m: 2 }}>
+      <Typography>Enter 8 digit product ID:</Typography>
+      <TextField
+        id="outlined-basic"
+        label="12345678"
+        variant="outlined"
+        onChange={(e) => setInput(e.target.value)}
+      />
+
+      <Button
+        variant="contained"
+        onClick={() => {
+          fetchData(input);
+        }}
+      >
+        Scan
+      </Button>
+      <Typography>{data}</Typography>
+      <Typography>{input}</Typography>
+    </Box>
+  );
+};
